2d13b67974e4babc6594d5f5ea06a88636540289
callyral <125226346+callyral@users.noreply.github.com>
2023-11-15 20:32:49 -0300

    Add help menu descriptions

--- src/arguments.rs
+++ src/arguments.rs
@@ -2,15 +2,15 @@ use clap::Parser;
 
 #[derive (Parser, Debug)]
 pub struct Arguments {
-    // Select color
+    /// Select color
     #[arg(short, long, default_value_t = String::from("blue"))]
     pub color: String,
 
-    // Disable bold colors
+    /// Disable bold colors
     #[arg(short, long)]
     pub no_bold: bool,
 
-    // Long kernel version
+    /// Long kernel version
     #[arg(short, long)]
     pub kernel_long: bool
 }
